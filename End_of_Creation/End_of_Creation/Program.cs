591a6a2fb564917930d6e3e40776cf20fbb2268d
Rocket6488 <197997@friscoisd.org>
2017-04-26 09:39:55 -0500

    First Commit

---
+++ End_of_Creation/End_of_Creation/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace End_of_Creation
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
